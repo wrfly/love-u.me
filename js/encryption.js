36442f84f2738a4594d5ff3c8cdd38aa0f21a1ff
wrfly <mr.fonely@gmail.com>
2014-11-06 23:13:47 +0800

    copyright

---
+++ js/encryption.js
@@ -0,0 +1,18 @@
+function password() {
+var testV = 1;
+var pass1 = prompt('请输入密码(love-u):','');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1 == "love-u")//自定义密码
+break;
+
+testV+=-1;
+var pass1 = 
+prompt('密码错误!请重新输入:');
+}
+if (pass1!="password" & testV ==testV+1)               
+history.go(-1);
+return " ";
+}                                                
+document.write(password());
